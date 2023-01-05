ace2cd1ebe39b22ffb10c753ea12d4b0f2a4ee3a
DanilT7 <dan20001410@gmail.com>
2023-01-05 20:20:09 +0200

    init

---
+++ main.js
@@ -0,0 +1,45 @@
+'use strict'
+const birthYear = Number(prompt('Введіть рік народження'));
+const city = prompt('У якому місті ви живете?');
+const sport = prompt('Ваш улюбленний вид спорту?');
+
+const currentYear = new Date().getFullYear();
+
+let age;
+if(birthYear === 0) {
+    age = 'Шкода, що Ви не захотіли ввести свою дату народження';
+} else {
+    age = `${currentYear - birthYear - 1} або ${currentYear - birthYear}`;
+}
+
+let userCity;
+if (city === 'Київ') {
+    userCity = 'Ти живеш у столиці України';
+} else if (city === 'Вашингтон') {
+    userCity = 'Ти живеш у столиці Сша';
+} else if (city === 'Лондон') {
+    userCity = 'Ти живеш у столиці Великобританії';
+} else if(city === null) {
+    userCity = 'Шкода, що Ви не захотіли ввести своє місто'
+} else {
+    userCity = 'Ти живеш у місті ' + city;
+}
+
+let userSport;
+if (sport === 'Футбол') {
+    userSport = 'Круто! Хочеш стати Ліонелєм Мессі?';
+} else if (sport === 'Теніс') {
+    userSport = 'Круто! Хочеш стати Рафаелєм Надалєм?';
+} else if (sport === 'Баскетбол') {
+    userSport = 'Круто! Хочеш стати Джеймсоном Лєброном,';
+} else if (sport === null) {
+    userSport = 'Шкода, що Ви не захотіли ввести свій улюбленний спорт';
+} else {
+    userSport = `Сподіваюсь ти станеш гарним гравцем у ${sport}`;
+}
+
+
+alert(`${age}\n
+${userCity} \n
+${userSport}`);
+
